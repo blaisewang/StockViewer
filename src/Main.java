217631b70bfad837423ae2a05877662f1d9d8f80
blaisewang <kaito.kaito.k@gmail.com>
2019-01-02 20:46:27 +0000

    Java FX

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Button Experiment 1");
+
+        DatePicker startDatePicker = new DatePicker();
+
+        DatePicker endDatePicker = new DatePicker();
+
+
+        HBox hbox = new HBox(startDatePicker, endDatePicker);
+
+        Scene scene = new Scene(hbox, 600, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        Application.launch(args);
     }
+
 }
